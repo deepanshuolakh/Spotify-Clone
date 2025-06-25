c780e4ab01de108f3c0cea397b530bdcae7e29ee
deepanshuolakh <deepanshuolakh@gmail.com>
2025-06-25 20:50:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,19 +17,23 @@ function secondsToMinutesSeconds(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("./songs/")
-    let response = await a.text();
-    // console.log(response);
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let songs = [];
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith("mp3")) {
-            songs.push(element.href.split("songs/")[1].split(".mp3")[0])
-        }
-    }
+    let songs = [
+        "52 Bars",
+        "Adhiya", 
+        "Admirin You",
+        "Chitta Kurta",
+        "Chithiyaan", 
+        "Gangsta",
+        "God Damn", 
+        "Hukam",
+        "Jee Ni Lagda", 
+        "Players",
+        "White Brown Black", 
+        "Ykwim",
+        "Winning Speech", 
+        
+        // Add all your song names here (without .mp3 extension)
+    ];
     return songs;
 
 }
