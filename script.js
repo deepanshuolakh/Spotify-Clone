4623dc46eeacd552e27f993b3c9efd4770958333
deepanshuolakh <deepanshuolakh@gmail.com>
2025-06-25 20:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let a = await fetch("songs/")
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div");
